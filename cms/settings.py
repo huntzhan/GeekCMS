b8d3a2a9c404e3064e123b0737627469ea7da05c
Haoxun Zhan <peter@haoxun.local>
2013-11-24 19:08:09 +0800

    clean setting.py

--- cms/settings.py
+++ cms/settings.py
@@ -1,13 +1,4 @@
-#ARTICLE_DIR = '/Users/peter/Data/Project/GeekCMS/GeekCMS/Article'
-#
-#NORMAL_DIR = 'articles'
-#
-#TEMPLATE_DIR = '/Users/peter/Data/Project/GeekCMS/GeekCMS/templates'
-#
-#OUTPUT_DIR = '/Users/peter/Data/Project/GeekCMS/GeekCMS/output'
-
-
-ARTICLE_DIR = '/Users/peter/Data/Project/haoxun.github.io/Article'
-NORMAL_DIR = 'articles'
-TEMPLATE_DIR = '/Users/peter/Data/Project/GeekCMS/GeekCMS/templates'
-OUTPUT_DIR = '/Users/peter/Data/Project/haoxun.github.io'
+ARTICLE_DIR = ''
+NORMAL_DIR = ''
+TEMPLATE_DIR = ''
+OUTPUT_DIR = ''
