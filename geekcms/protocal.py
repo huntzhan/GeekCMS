1f554aa0b119819469e5502322e6d692db53b7a6
詹昊恂 <programmer.zhx@gmail.com>
2014-03-05 01:00:27 +0800

    Add plugin registration

--- geekcms/protocal.py
+++ geekcms/protocal.py
@@ -1,4 +1,14 @@
 from collections import defaultdict
+from collections import UserDict
+from functools import partial
+from functools import wraps
+from inspect import signature
+
+
+_THEME = 'theme'
+_PLUGIN = 'plugin'
+_PLUGIN_RUN_METHOD_NAME = 'run'
+_OWNER = 'owner'
 
 
 class _Manager:
@@ -22,6 +32,22 @@ class _Manager:
         return self._container[owner]
 
 
+class _ManagerProxyWithOwner:
+
+    def __init__(self, owner, manager):
+
+        for method_name in dir(manager):
+            if method_name.startswith('_'):
+                continue
+            method = getattr(manager, method_name)
+            sig = signature(method)
+            if _OWNER in sig.parameters:
+                partial_method = partial(method, owner=owner)
+            else:
+                partial_method = method
+            setattr(self, method_name, partial_method)
+
+
 class _SetUpObjectManager(type):
 
     def __new__(cls, *args, **kwargs):
@@ -34,10 +60,14 @@ class _SetUpObjectManager(type):
 class _Base(metaclass=_SetUpObjectManager):
 
     def __init__(self, owner, *args, **kwargs):
-        text = 'In Base Class: *args: {}, {}; **kwargs: {}'
+        text = 'In Base Class: owner: {}, *args: {}; **kwargs: {}'
         raise Exception(
             text.format(owner, args, kwargs),
         )
+
+    @classmethod
+    def get_manager_with_fixed_owner(cls, owner):
+        return _ManagerProxyWithOwner(owner, cls.objects)
 
 
 class Resource(_Base):
@@ -50,3 +80,59 @@ class Product(_Base):
 
 class Message(_Base):
     pass
+
+
+class _UniqueKeyDict(UserDict):
+
+    def __setitem__(self, key, val):
+        if key in self:
+            raise Exception('Key Already Existed!.')
+        super().__setitem__(key, val)
+
+
+class _SetUpPlugin(type):
+
+    plugin_mapping = _UniqueKeyDict()
+
+    @classmethod
+    def _find_case_insensitive_name(cls, target_name, namespace):
+        for name, val in namespace.items():
+            if name.lower() == target_name:
+                return name
+        else:
+            raise Exception(
+                'Can Not Find {}.'.format(target_name),
+            )
+
+    @classmethod
+    def _register_plugin(cls, theme_name, plugin_name, plugin_cls):
+        unique_key = '{}.{}'.format(theme_name, plugin_name)
+        cls.plugin_name[unique_key] = plugin_cls
+
+    @classmethod
+    def _data_filter(cls, func=None, owner=''):
+        if func is None:
+            return partial(cls._data_filter, owner=owner)
+
+        # begin decorating
+        @wraps(func)
+        def run(self, data, *args, **kwargs):
+            processed_data = [item for item in data if data.owner == owner]
+            return func(self, processed_data, *args, **kwargs)
+        return run
+
+    def __new__(cls, cls_name, bases, namespace, **kargs):
+        # find theme_name and plugin_name
+        theme_name = cls._find_case_insensitive_name(_THEME, namespace)
+        plugin_name = cls._find_case_insensitive_name(_PLUGIN, namespace)
+        # generate class(plugin)
+        plugin_cls = type.__new__(cls, cls_name, bases, namespace, **kargs)
+        # register plugin
+        cls._register_plugin(theme_name, plugin_name, plugin_cls)
+        # filter data for run method.
+        process_func = getattr(plugin_cls, _PLUGIN_RUN_METHOD_NAME)
+        setattr(
+            plugin_cls,
+            _PLUGIN_RUN_METHOD_NAME,
+            cls._data_filter(owner=theme_name)(process_func),
+        )
